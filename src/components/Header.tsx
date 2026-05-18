3a7a7f4593409d2d40402528f5c5a6e7248ba405
YuhaoSUN <yuhao.sun@vml.com>
2026-05-18 11:36:00 +0200

    UPDATE: replace logo.svg with logo-new.png in public/img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,10 +12,10 @@ export default function Header({ compact }: HeaderProps) {
     <header className={`${styles.header} ${compact ? styles.compact : ''}`}>
       <div className={styles.logo}>
         <Image 
-          src="/icons/logo.svg" 
-          alt="AudioNova" 
-          width={163} 
-          height={32} 
+          src="/img/logo-new.png"
+          alt="AuditionSanté"
+          width={163}
+          height={32}
           priority 
         />
       </div>
